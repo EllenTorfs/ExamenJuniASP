af93d8da373f68b47588041838e11a03ef01be19
Ellen Torfs <u0068049@thomasmore.be>
2020-04-03 12:16:47 +0200

    Add project files.

---
+++ ExamenJuniASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExamenJuniASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
